98f801e6a5f0439915af913558f93092d660eaf7
kaydejay <109537825+kaydejay@users.noreply.github.com>
2025-01-20 20:49:44 -0600

    Create Training JS #4: Basic data types--Array.js

---
+++ Training JS #4: Basic data types--Array.js
@@ -0,0 +1,36 @@
+function getLength(arr){
+2
+  return arr.length
+3
+}
+4
+function getFirst(arr){
+5
+  return arr[0]
+6
+}
+7
+function getLast(arr){
+8
+  return arr[arr.length-1]
+9
+}
+10
+function pushElement(arr){
+11
+  var el=1
+12
+  arr.push(el)
+13
+  return arr
+14
+}
+15
+function popElement(arr){
+16
+  arr.pop()
+17
+  return arr;
+18
+}
+ Great! You may ta
