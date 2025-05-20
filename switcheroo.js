58de4fb0f9df77f8a92be654aa6961463f3d488e
kaydejay <109537825+kaydejay@users.noreply.github.com>
2025-05-19 19:20:37 -0500

    Create switcheroo.js

---
+++ switcheroo.js
@@ -0,0 +1,11 @@
+function switcheroo(x){
+  return x.split('').map(char => {
+    if (char === 'a') {
+      return 'b';
+    } else if (char === 'b') {
+      return 'a';
+    } else {
+      return char;
+    }
+  }).join('');
+}
