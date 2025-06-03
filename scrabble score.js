2b147baaf186b8e334ff9c9672c7c90a7f9fdab9
kaydejay <109537825+kaydejay@users.noreply.github.com>
2025-06-02 20:40:12 -0500

    Update scrabble score.js

--- scrabble score.js
+++ scrabble score.js
@@ -15,3 +15,12 @@ function scrabbleScore(str){
   }
   return output;
 }
+
+function scrabbleScore(str){
+  let upperStr = str.toUpperCase();
+  let sum = 0;
+  for (let i = 0; i < str.length; i++){
+    sum += $dict[upperStr[i]] || 0;
+  }
+  return sum
+}
