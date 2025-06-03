3b849b93b9b94b95ca639866b6299dd005e6042e
kaydejay <109537825+kaydejay@users.noreply.github.com>
2025-06-02 20:46:05 -0500

    Update switcheroo.js

--- switcheroo.js
+++ switcheroo.js
@@ -24,3 +24,17 @@ function switcheroo(x){
   }
   return newX
 }
+
+function switcheroo(x){
+  let result = ''
+  for (let i = 0; i < x.length; i++){
+    if (x[i] == 'a'){
+      result += 'b'
+    }else if (x[i] == 'b'){
+      result += 'a'
+    }else{
+      result += 'c'
+    }
+  }
+  return result
+}
