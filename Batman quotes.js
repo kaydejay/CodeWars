778585b03ec913bf431da6c9a39b8a571241c504
kaydejay <109537825+kaydejay@users.noreply.github.com>
2025-06-30 18:04:24 -0500

    Create Batman quotes.js

---
+++ Batman quotes.js
@@ -0,0 +1,14 @@
+let getQuote = function(quotes, hero){
+  let names = {
+    R : 'Robin',
+    J : 'Joker',
+    B : 'Batman'
+  }
+  let number = "";
+    for (let i = 0; i < hero.length; i++) {
+        if (!isNaN(hero[i])) {
+            number += hero[i];
+        }
+    }
+   return `${names[hero[0]]}: ${quotes[number]}`  
+}
