4cd4f3a7b5f3bc17387ae5e6ed444685a192fda5
kaydejay <109537825+kaydejay@users.noreply.github.com>
2025-08-13 20:33:06 -0500

    Create find multiples of a number 8 kyu.js

---
+++ find multiples of a number 8 kyu.js
@@ -0,0 +1,12 @@
+function findMultiples(integer, limit) {
+  let arr = []
+  
+  for (let i = 1; i <= limit; i++){
+    if (integer * i > limit){
+      break
+    }
+    arr.push(integer * i);
+  }
+
+  return arr
+}
