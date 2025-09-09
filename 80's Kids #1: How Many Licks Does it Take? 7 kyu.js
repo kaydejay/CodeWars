a316441628c5e7cf19149aeb7885865ef2dc2e69
kaydejay <109537825+kaydejay@users.noreply.github.com>
2025-09-08 21:16:03 -0500

    Create 80's Kids #1: How Many Licks Does it Take? 7 kyu.js

---
+++ 80's Kids #1: How Many Licks Does it Take? 7 kyu.js
@@ -0,0 +1,10 @@
+function totalLicks(env) {
+  let objArray = Object.entries(env);
+  
+  let sum = objArray.reduce((acc, cur) => acc + cur[1],252);
+  let sorted = objArray.sort((a,b) => b[1] - a[1]);
+
+  let answer = `It took ${sum} licks to get to the tootsie roll center of a tootsie pop.`
+
+  return sorted.length == 0 || sorted[0][1] < 0 ? answer : `${answer} The toughest challenge was ${sorted[0][0]}.`; 
+}
