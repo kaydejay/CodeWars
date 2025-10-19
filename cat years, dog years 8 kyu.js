e60ca778f7fd5f40c891396b08c51e9d2ebb6a63
kaydejay <109537825+kaydejay@users.noreply.github.com>
2025-10-19 15:03:51 -0500

    Create cat years, dog years 8 kyu.js

---
+++ cat years, dog years 8 kyu.js
@@ -0,0 +1,8 @@
+  if (humanYears == 1){
+    return [humanYears, 15, 15]
+  }else if (humanYears == 2){
+    return [humanYears, 24, 24]
+  }else{
+    return [humanYears, 24 + ((humanYears - 2) * 4), 24 + ((humanYears - 2) * 5)];
+  }
+}
