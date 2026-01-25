94cc1f3da9505990cc198ae8aa05d20cf0a13e15
kaydejay <109537825+kaydejay@users.noreply.github.com>
2026-01-24 23:36:01 -0600

    Implement uefaEuro2016 function to determine match result

---
+++ UEFA EURO 2016 8 kyu.js
@@ -0,0 +1,11 @@
+function uefaEuro2016(teams, scores){
+  for (let i = 0; i < scores.length; i++){
+    if (scores[0] > scores[1]){
+      return `At match ${teams[0]} - ${teams[1]}, ${teams[0]} won!`
+    }else if (scores[1] > scores[0]){
+      return `At match ${teams[0]} - ${teams[1]}, ${teams[1]} won!`
+    }else{
+      return `At match ${teams[0]} - ${teams[1]}, teams played draw.`
+    }
+  }
+}
